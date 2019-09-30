a70533b642cbf1c8d20fb8e767bbc4691f089d95
Dat Tran <datitran@gmail.com>
2019-09-30 16:00:33 +0200

    Fix cp command in building the documentation.

--- mkdocs/run_docs.sh
+++ mkdocs/run_docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp ../README.md docs/index.md
-cp ../_readme/ docs/_readme/
+cp -a ../_readme/ docs/_readme/
 cp ../CONTRIBUTING.md docs/CONTRIBUTING.md
 cp ../LICENSE docs/LICENSE.md
 python autogen.py
