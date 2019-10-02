b2f176f45284e9779737ca216d5ae4b0855a9ffd
cschaefer26 <c.schaefer.home@gmail.com>
2019-10-02 18:22:41 +0200

    Optional eager and static execution.

--- headliner/training.py
+++ headliner/training.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     data_raw = read_data('/Users/cschaefe/datasets/en_ger.txt')[0:10000]
     train_data, val_data = train_test_split(data_raw, test_size=100, shuffle=True, random_state=42)
     summarizer = SummarizerAttention(lstm_size=256, embedding_size=50)
-    trainer = Trainer(steps_per_epoch=100, batch_size=16, steps_to_log=5, tensorboard_dir='/tmp/padded', max_output_len=30)
+    trainer = Trainer(steps_per_epoch=100, batch_size=16, steps_to_log=5, tensorboard_dir='/tmp/padded', max_output_len=None)
     trainer.train(summarizer, train_data, val_data=val_data, scorers={'bleu': BleuScorer(weights=(1, 0, 0, 0))})
 
     """
