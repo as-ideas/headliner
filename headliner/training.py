ecd63eb7169d2220aa5daf1946501138ce646f32
cschaefer26 <c.schaefer.home@gmail.com>
2019-10-07 17:11:20 +0200

    Minor refactoring.

--- headliner/training.py
+++ headliner/training.py
@@ -35,7 +35,8 @@ if __name__ == '__main__':
     train_data, val_data = train_test_split(data_raw, test_size=500, shuffle=True, random_state=42)
     #summarizer = SummarizerAttention(max_prediction_len=12, lstm_size=256, embedding_size=50)
 
-    summarizer = SummarizerTransformer(num_heads=1,
+    summarizer = SummarizerTransformer(num_heads=8,
+                                       feed_forward_dim=1024,
                                        embedding_size=50,
                                        embedding_encoder_trainable=True,
                                        embedding_decoder_trainable=True,
@@ -44,8 +45,8 @@ if __name__ == '__main__':
     trainer = Trainer(steps_per_epoch=500,
                       batch_size=8,
                       steps_to_log=5,
-                     # glove_path_encoder='/Users/cschaefe/datasets/glove_welt_dedup.txt',
-                    #  glove_path_decoder='/Users/cschaefe/datasets/glove_welt_dedup.txt',
+                      glove_path_encoder='/Users/cschaefe/datasets/glove_welt_dedup.txt',
+                      glove_path_decoder='/Users/cschaefe/datasets/glove_welt_dedup.txt',
                       tensorboard_dir='/tmp/trans_emb',
                       max_output_len=20)
 
