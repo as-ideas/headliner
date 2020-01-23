9e3cdb4535283d33f76d4f1a2ecf9dac0991eebb
cschaefer26 <c.schaefer.home@gmail.com>
2020-01-23 16:45:18 +0100

    Reverted loss masking due to problems with attention summarizer.

--- headliner/losses.py
+++ headliner/losses.py
@@ -2,11 +2,8 @@ import tensorflow as tf
 
 
 def masked_crossentropy(targets: tf.Tensor, logits: tf.Tensor) -> tf.Tensor:
-    # need this to fix wrong normalization of out-of-box SparseCategoricalCrossentropy
-    crossentropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True,
-                                                                 reduction=tf.keras.losses.Reduction.SUM)
+    crossentropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     mask = tf.math.logical_not(tf.math.equal(targets, 0))
-    mask = tf.cast(mask, dtype=tf.int32)
-    mask_sum = tf.cast(tf.reduce_sum(mask), dtype=tf.float32)
-    loss_sum = crossentropy(targets, logits, sample_weight=mask)
-    return loss_sum / mask_sum
+    mask = tf.cast(mask, dtype=tf.int64)
+    loss = crossentropy(targets, logits, sample_weight=mask)
+    return loss
