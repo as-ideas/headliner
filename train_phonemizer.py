119f884dede2672bff031944b063aff4bf294066
cschaefer26 <c.schaefer.home@gmail.com>
2021-01-25 12:50:29 +0100

    Lowered lr.

--- train_phonemizer.py
+++ train_phonemizer.py
@@ -1,6 +1,10 @@
 import json
 import re
+import tensorflow as tf
+from functools import _lru_cache_wrapper
 from random import Random
+
+from thinc.neural.optimizers import Adam
 
 from headliner.model.transformer_summarizer import TransformerSummarizer
 from headliner.trainer import Trainer
@@ -43,13 +47,13 @@ if __name__ == '__main__':
     random.shuffle(train_data)
     val_data, train_data = train_data[:1000], train_data[1000:]
     print(f'train: {len(train_data)}, val: {len(val_data)}, max pred len: {max_len}')
-
     summarizer = TransformerSummarizer(num_heads=4,
                                        feed_forward_dim=1024,
                                        num_layers=4,
                                        embedding_size=512,
                                        dropout_rate=0.,
                                        max_prediction_len=max_len+2)
+    summarizer.optimizer = tf.keras.optimizers.Adam(1e-4)
     trainer = Trainer(batch_size=32,
                       steps_per_epoch=500,
                       max_vocab_size_encoder=10000,
